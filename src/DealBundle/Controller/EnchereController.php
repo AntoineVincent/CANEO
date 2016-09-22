4acd6ea8343086f90c9cba404a3a91ac1fa77dad
Antoine <anton51200@laposte.net>
2016-09-22 11:40:49 +0200

    create page for enchere, update function, and complete data

--- src/DealBundle/Controller/EnchereController.php
+++ src/DealBundle/Controller/EnchereController.php
@@ -21,17 +21,68 @@ class EnchereController extends Controller
         foreach($encheres as $enchere) {
         	$product = $em->getRepository('ProductBundle:Produit')->findOneById($enchere->getIdproduit());
         	$fournisseur = $em->getRepository('AppBundle:User')->findOneById($enchere->getIdfournisseur());
-
+        	$commande = $em->getRepository('DealBundle:Commandes')->findBy(array(
+	        	'idenchere' => $enchere->getId(),
+	        	'idacheteur' => $user->getId(),
+	        ));
+	        if ($commande == NULL) {
+	        	$commandeUser = "Pas de commandes";
+	        }
+	        else {
+	        	$commandeUser = $commande->getNbredecommande();
+	        }
         	$tabenchere [] = array (
+        		'id' => $enchere->getId(),
         		'prix' =>  $enchere->getPrix(),
         		'logoFournisseur' => $fournisseur->getLogo(),
         		'produit' => $product->getNom(),
         		'totalCommande' => $enchere->getTotalcommande(),
+        		'commandeUser' => $commandeUser,
         	);
         }
 
 
         return $this->render('encheres/all_enchere.html.twig', array(
+            'user' => $user,
+            'tabenchere' => $tabenchere,
+        ));
+    }
+
+    public function ficheEnchereAction(Request $request, $idenchere)
+    {
+    	$em = $this->getDoctrine()->getManager();
+    	$user = $this->container->get('security.context')->getToken()->getUser();
+
+        $enchere = $em->getRepository('DealBundle:Encheres')->findOneById($idenchere);
+        $commande = $em->getRepository('DealBundle:Commandes')->findBy(array(
+        	'idenchere' => $idenchere,
+        	'idacheteur' => $user->getId(),
+        ));
+        if ($commande == NULL) {
+        	$commandeUser = "Pas de commandes";
+        }
+        else {
+        	$commandeUser = $commande->getNbredecommande();
+        }
+        $product = $em->getRepository('ProductBundle:Produit')->findOneById($enchere->getIdproduit());
+        $fournisseur = $em->getRepository('AppBundle:User')->findOneById($enchere->getIdfournisseur());
+        $nbreAcheteur = count($em->getRepository('DealBundle:Commandes')->findByIdenchere($idenchere));
+
+        $tabenchere [] = array (
+        	'id' => $enchere->getId(),
+        	'prix' =>  $enchere->getPrix(),
+        	'logoFournisseur' => $fournisseur->getLogo(),
+        	'idFournisseur' => $fournisseur->getId(),
+        	'cgv' => $fournisseur->getInfos(),
+        	'produit' => $product->getNom(),
+        	'descProduit' => $product->getDescription(),
+        	'photoProduit' => $product->getPhoto(),
+        	'totalCommande' => $enchere->getTotalcommande(),
+        	'commandeUser' => $commandeUser,
+        	'nbreAcheteur' => $nbreAcheteur,
+        );
+
+        return $this->render('encheres/fiche_enchere.html.twig', array(
             'user' => $user,
             'tabenchere' => $tabenchere,
         ));
