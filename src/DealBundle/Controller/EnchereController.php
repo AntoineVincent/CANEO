38bb18146f9c890c378bb9ed46e159665b61107f
Antoine <anton51200@laposte.net>
2016-09-22 12:21:26 +0200

    correct some issues

--- src/DealBundle/Controller/EnchereController.php
+++ src/DealBundle/Controller/EnchereController.php
@@ -21,7 +21,7 @@ class EnchereController extends Controller
         foreach($encheres as $enchere) {
         	$product = $em->getRepository('ProductBundle:Produit')->findOneById($enchere->getIdproduit());
         	$fournisseur = $em->getRepository('AppBundle:User')->findOneById($enchere->getIdfournisseur());
-        	$commande = $em->getRepository('DealBundle:Commandes')->findBy(array(
+        	$commande = $em->getRepository('DealBundle:Commandes')->findOneBy(array(
 	        	'idenchere' => $enchere->getId(),
 	        	'idacheteur' => $user->getId(),
 	        ));
@@ -48,13 +48,64 @@ class EnchereController extends Controller
         ));
     }
 
+    public function myEnchereAction(Request $request)
+    {
+    	$em = $this->getDoctrine()->getManager();
+    	$user = $this->container->get('security.context')->getToken()->getUser();
+
+    	$tabenchere = [];
+    	$encheres = [];
+
+    	if($user->getType() == "acheteur") {
+    		$commandeforSearch = $em->getRepository('DealBundle:Commandes')->findByIdacheteur($user->getId());
+    		foreach($commandeforSearch as $com) {
+    			$enchere = $em->getRepository('DealBundle:Encheres')->findOneById($com->getIdenchere());
+    			array_push($encheres, $enchere);
+    		}
+    	}
+    	elseif ($user->getType() == "fournisseur") {
+    		$encheres = $em->getRepository('DealBundle:Encheres')->findByIdfournisseur($user->getId());
+    	}
+
+
+
+        foreach($encheres as $enchere) {
+        	$product = $em->getRepository('ProductBundle:Produit')->findOneById($enchere->getIdproduit());
+        	$fournisseur = $em->getRepository('AppBundle:User')->findOneById($enchere->getIdfournisseur());
+        	$commande = $em->getRepository('DealBundle:Commandes')->findOneBy(array(
+	        	'idenchere' => $enchere->getId(),
+	        	'idacheteur' => $user->getId(),
+	        ));
+	        if ($commande != NULL) {
+	        	$commandeUser = $commande->getNbredecommande();
+	        }
+	        else {
+	        	$commandeUser = "Pas de commandes";
+	        }
+        	$tabenchere [] = array (
+        		'id' => $enchere->getId(),
+        		'prix' =>  $enchere->getPrix(),
+        		'logoFournisseur' => $fournisseur->getLogo(),
+        		'produit' => $product->getNom(),
+        		'totalCommande' => $enchere->getTotalcommande(),
+        		'commandeUser' => $commandeUser,
+        	);
+        }
+
+
+        return $this->render('encheres/all_enchere.html.twig', array(
+            'user' => $user,
+            'tabenchere' => $tabenchere,
+        ));
+    }
+
     public function ficheEnchereAction(Request $request, $idenchere)
     {
     	$em = $this->getDoctrine()->getManager();
     	$user = $this->container->get('security.context')->getToken()->getUser();
 
         $enchere = $em->getRepository('DealBundle:Encheres')->findOneById($idenchere);
-        $commande = $em->getRepository('DealBundle:Commandes')->findBy(array(
+        $commande = $em->getRepository('DealBundle:Commandes')->findOneBy(array(
         	'idenchere' => $idenchere,
         	'idacheteur' => $user->getId(),
         ));
