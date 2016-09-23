10976a2208dfb3915f183412e34ad2f3a5d33470
Antoine <anton51200@laposte.net>
2016-09-23 13:52:31 +0200

    user can add/remove favoris

--- src/ProductBundle/Controller/DefaultController.php
+++ src/ProductBundle/Controller/DefaultController.php
@@ -6,6 +6,12 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use ProductBundle\Entity\Favoris;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
+
 
 class DefaultController extends Controller
 {
@@ -23,6 +29,12 @@ class DefaultController extends Controller
     		$product = $em->getRepository('ProductBundle:Produit')->findOneById($favori->getIdproduit());
 
 	        $enchere = $em->getRepository('DealBundle:Encheres')->findOneByIdproduit($product->getId());
+	        if($enchere != NULL){
+	        	$idenchere = $enchere->getId();
+	        }
+	        else {
+	        	$idenchere = "non";
+	        }
 
 	        $tabproducts[] = array(
 	            'id' => $product->getId(),
@@ -30,7 +42,7 @@ class DefaultController extends Controller
 	            'prixminimal' => $product->getPrixminimal(),
 	            'commandemaximal' => $product->getCommandemaximal(),
 	            'enchere' => $enchere,
-	            'idenchere' => $enchere->getId(),
+	            'idenchere' => $idenchere,
 	        );
     	}
 
@@ -39,5 +51,49 @@ class DefaultController extends Controller
         	'user' =>$user,
         	'products' => $tabproducts,
         ));
+    }
+
+    public function addDeleteFavAction(Request $request)
+    {
+    	$em = $this->getDoctrine()->getManager();
+    	$user = $this->container->get('security.context')->getToken()->getUser();
+
+    	$idproduit = $this->getRequest()->request->get('idproduit');
+
+    	$verifFav = $em->getRepository('ProductBundle:Favoris')->findOneBy(array(
+    		'idacheteur' => $user->getId(),
+    		'idproduit' => $idproduit,
+    	));
+
+    	if($verifFav != NULL) {
+    		$em->remove($verifFav);
+    		$em->flush();
+	    	
+	    	$msg = "remove";
+    	}
+    	else {
+    		$favoris = new Favoris();
+
+	    	$favoris->setIdacheteur($user->getId());
+	    	$favoris->setIdproduit($idproduit);
+
+	    	$em->persist($favoris);
+	    	$em->flush();
+
+	    	$msg = "added";
+    	}
+
+    	$tab = [];
+        $tab[] = array(
+            'message' => $msg,
+            );
+        $encoders = array(new XmlEncoder(), new JsonEncoder());
+        $normalizers = array(new GetSetMethodNormalizer());
+        $serializer = new Serializer($normalizers, $encoders);
+
+        $jsonContent = $serializer->serialize($tab, 'json');
+
+        $response = new Response($jsonContent);
+        return $response;
     }
 }
