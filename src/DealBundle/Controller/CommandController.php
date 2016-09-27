d9543d647eefe09f4843c4a9c473f7216bcc7078
Antoine <anton51200@laposte.net>
2016-09-27 13:16:58 +0200

    update ajax fiunction, add instance for direct cmd on dashboard

--- src/DealBundle/Controller/CommandController.php
+++ src/DealBundle/Controller/CommandController.php
@@ -6,6 +6,11 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use DealBundle\Entity\Commandes;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
 
 class CommandController extends Controller
 {
@@ -80,5 +85,63 @@ class CommandController extends Controller
             'enchere' => $enchere,
             'product' => $product,
         ));
+    }
+
+    public function newCmdAjaxAction(Request $request) 
+    {
+    	$em = $this->getDoctrine()->getManager();
+    	$user = $this->container->get('security.context')->getToken()->getUser();
+
+    	$idenchere = $request->request->get('idenchere');
+    	$nbrecmd = $request->request->get('nbrecmd');
+
+    	$cmd = $em->getRepository('DealBundle:Commandes')->findOneBy(array(
+	    	'idacheteur' => $user->getId(),
+	    	'idenchere' => $idenchere,
+	    ));
+	    $enchere = $em->getRepository('DealBundle:Encheres')->findOneById($idenchere);
+	    $product = $em->getRepository('ProductBundle:Produit')->findOneById($enchere->getIdproduit());
+
+    		if($nbrecmd != NULL) {
+    			if($nbrecmd < $product->getCommandemaximal()) {
+    				$result = "Quantité insuffisante !";
+    			}
+    			else {
+		    		if($cmd != NULL) {
+		    			$cmd->setNbredecommande($cmd->getNbredecommande() + $nbrecmd);
+		    			$enchere->setTotalcommande($enchere->getTotalcommande() + $nbrecmd);
+
+		    			$em->persist($cmd);
+		    			$em->flush();
+		    			$em->persist($enchere);
+		    			$em->flush();
+
+		    			$result = "Votre commande de ".$nbrecmd." a bien été enregistré.";
+		    		}
+		    		else {
+		    			$cmd = new Commandes();
+		    			$cmd->setIdacheteur($user->getId());
+		    			$cmd->setIdenchere($idenchere);
+		    			$cmd->setNbredecommande($nbrecmd);
+		    			$enchere->setTotalcommande($enchere->getTotalcommande() + $nbrecmd);
+
+		    			$em->persist($cmd);
+		    			$em->flush();
+		    			$em->persist($enchere);
+		    			$em->flush();
+
+		    			$result = "Votre commande de ".$nbrecmd." a bien été enregistré.";
+		    		}
+		    	}
+		    }
+
+    	$encoders = array(new XmlEncoder(), new JsonEncoder());
+        $normalizers = array(new GetSetMethodNormalizer());
+        $serializer = new Serializer($normalizers, $encoders);
+
+        $jsonContent = $serializer->serialize($result, 'json');
+
+        $response = new Response($jsonContent);
+        return $response;
     }
 }
